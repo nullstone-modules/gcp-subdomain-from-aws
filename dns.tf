66ef9fab98728a5847f93f11d02c446b149f2263
Brad Sickles <brad.sickles@gmail.com>
2023-05-09 09:46:08 -0400

    Enable gcp apis before performing actions (#1)

--- dns.tf
+++ dns.tf
@@ -1,7 +1,15 @@
+resource "google_project_service" "dns" {
+  service                    = "dns.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
 resource "google_dns_managed_zone" "this" {
   name     = local.block_ref
   dns_name = local.fqdn
   labels   = local.labels
+
+  depends_on = [google_project_service.dns]
 }
 
 resource "aws_route53_record" "this-delegation" {
