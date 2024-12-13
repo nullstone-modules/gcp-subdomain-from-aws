82701c97e4af6039f7a95d4e862ff1b5dbb0c97f
Brad Sickles <brad.sickles@gmail.com>
2024-12-13 10:11:26 -0500

    release

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Nullstone module to create a GCP-managed subdomain using an AWS domain.
 
 This module creates a DNS Zone on Google Cloud DNS.
 
+By default, this module will create an SSL certificate using GCP Certificate Manager.
+This can be disabled by setting the variable `disable_certificate = true`.
+This certificate is created for Cloud CDNs and Load Balancing (most common use case).
+A certificate and certificate map are created and emitted as outputs for use to attach to a GCP Load Balancer.
+
 The `domain` connection refers to a Nullstone domain that is AWS-managed.
 This module performs delegation -- it will add records to the root domain in AWS to direct the subdomain to newly-created GCP DNS Zone.
 This allows a user to manage this subdomain fully in GCP without having access to the root domain in AWS.
--- variables.tf
+++ variables.tf
@@ -15,21 +15,3 @@ variable "disable_certificate" {
   default     = false
   description = "Specify true to disable SSL certificate creation"
 }
-
-variable "certificate_purpose" {
-  type        = string
-  default     = "load-balancer"
-  description = <<EOF
-This enables creating a GCP-managed SSL certificate in Certificate Manager.
-The purpose allows you to configure the certificate for the device
-Available choices: "cdn" for Cloud CDN, "load-balancer" for Internet Load Balancers, or "internal"
-EOF
-
-  validation {
-    condition     = contains(["cdn", "load-balancer", "internal"], var.certificate_purpose)
-    error_message = <<EOF
-The purpose must be one of 'cdn', 'load-balancer', or 'internal'.
-This is meant to create a certificate that is specific to the ingress device that it is attached.
-EOF
-  }
-}
