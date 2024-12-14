c7feecb1777c670e8d70ebb422f463e95e097b93
Brad Sickles <brad.sickles@gmail.com>
2024-12-14 17:04:28 -0500

    Added optional certificate connection

--- cert.tf
+++ cert.tf
@@ -1,8 +1,22 @@
+data "ns_connection" "certificate" {
+  name     = "certificate"
+  contract = "block/gcp/certificate-manager"
+  optional = true
+}
+
+locals {
+  ext_certificate_id       = try(data.ns_connection.certificate.outputs.certificate_id, "")
+  ext_certificate_map_id   = try(data.ns_connection.certificate.outputs.certificate_map_id, "")
+  ext_certificate_map_name = try(data.ns_connection.certificate.outputs.certificate_map_name, "")
+
+  create_cert = !var.disable_certificate && local.ext_certificate_map_id == ""
+}
+
 module "cert" {
   source  = "nullstone-modules/sslcert/gcp"
   version = "~> 0.1.0"
 
-  enabled = !var.disable_certificate
+  enabled = local.create_cert
   name    = local.resource_name
   labels  = local.labels
   scope   = ""
@@ -13,7 +27,7 @@ module "cert" {
 }
 
 locals {
-  certificate_id       = module.cert.certificate_id
-  certificate_map_id   = module.cert.certificate_map_id
-  certificate_map_name = module.cert.certificate_map_name
+  certificate_id       = coalesce(local.ext_certificate_id, module.cert.certificate_id)
+  certificate_map_id   = coalesce(local.ext_certificate_map_id, module.cert.certificate_map_id)
+  certificate_map_name = coalesce(local.ext_certificate_map_name, module.cert.certificate_map_name)
 }
